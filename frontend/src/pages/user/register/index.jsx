e897d77e5b72b98d90b5bb53c587fd0b06d80380
llf <llf@email.com>
2019-12-10 21:26:29 +0800

    修改了宽度

--- frontend/src/pages/user/register/index.jsx
+++ frontend/src/pages/user/register/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Input, Button, Row, Col, notification } from 'antd';
+import { Form, Input, Button, notification } from 'antd';
 import { connect } from 'dva';
 import { Link, router } from 'umi';
 
@@ -46,57 +46,55 @@ class Register extends Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
-      <Row type="flex" justify="center">
-        <Col span={4}>
-          <Form onSubmit={this.onRegister}>
-            <Form.Item>
-              {getFieldDecorator('name', {
-                rules: [{ required: true, message: '用户名不能为空' }],
-              })(<Input placeholder="用户名" />)}
-            </Form.Item>
+      <div style={{ margin: '0 auto', width: 300, maxWidth: '100vw' }}>
+        <Form onSubmit={this.onRegister}>
+          <Form.Item>
+            {getFieldDecorator('name', {
+              rules: [{ required: true, message: '用户名不能为空' }],
+            })(<Input placeholder="用户名" />)}
+          </Form.Item>
 
-            <Form.Item>
-              {getFieldDecorator('email', {
-                rules: [
-                  { required: true, message: '邮箱不能为空' },
-                  { pattern: /\w+@\w+(\.\w+){1,2}/, message: '邮箱不正确' },
-                ],
-              })(<Input placeholder="邮箱" />)}
-            </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('email', {
+              rules: [
+                { required: true, message: '邮箱不能为空' },
+                { pattern: /\w+@\w+(\.\w+){1,2}/, message: '邮箱不正确' },
+              ],
+            })(<Input placeholder="邮箱" />)}
+          </Form.Item>
 
-            <Form.Item>
-              {getFieldDecorator('password', {
-                rules: [
-                  { required: true, message: '密码不能为空' },
-                  { min: 6, message: '密码至少为6位组成' },
-                ],
-                validateFirst: true,
-              })(<Input.Password placeholder="密码" type="password" />)}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('c_password', {
-                rules: [
-                  {
-                    required: true,
-                    message: '请再次输入以确认新密码',
-                  },
-                  {
-                    validator: this.handleConfirmPassword,
-                  },
-                ],
-              })(<Input.Password placeholder="密码" type="password" />)}
-            </Form.Item>
-            <Form.Item>
-              <Button htmlType="submit" type="primary">
-                注册
-              </Button>
-              <Link style={{ float: 'right' }} to="/user/login">
-                去登录
-              </Link>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [
+                { required: true, message: '密码不能为空' },
+                { min: 6, message: '密码至少为6位组成' },
+              ],
+              validateFirst: true,
+            })(<Input.Password placeholder="密码" type="password" />)}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('c_password', {
+              rules: [
+                {
+                  required: true,
+                  message: '请再次输入以确认新密码',
+                },
+                {
+                  validator: this.handleConfirmPassword,
+                },
+              ],
+            })(<Input.Password placeholder="密码" type="password" />)}
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="submit" type="primary">
+              注册
+            </Button>
+            <Link style={{ float: 'right' }} to="/user/login">
+              去登录
+            </Link>
+          </Form.Item>
+        </Form>
+      </div>
     );
   }
 }

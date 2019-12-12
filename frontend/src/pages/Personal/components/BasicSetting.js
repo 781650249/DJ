e31016f02a0161543216b4109209e25abe051c59
llf <llf@email.com>
2019-12-12 17:05:50 +0800

    改了

--- frontend/src/pages/Personal/components/BasicSetting.js
+++ frontend/src/pages/Personal/components/BasicSetting.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
+import { Icon } from 'antd';
 
 @connect(({ user, loading }) => ({
   user,
   submitting: loading.effects['user/fetchCurrent'],
 }))
 export default class BasicSetting extends Component {
-  constructor(props) {
-    console.log(props);
-    super(props);
-  }
-
   render() {
     const {
       user: { currentUser },
     } = this.props;
     return (
       <div>
-        <div style={{ border: '1px dashed', width: 260, padding: '10px' }}>
-          <h3>用户名：{currentUser.name}</h3>
-          <h3>邮箱: {currentUser.email}</h3>
+        <div style={{ width: 260, padding: '10px' }}>
+          <h1>
+            <Icon type="user" />: {currentUser.name}
+          </h1>
+          <h1>
+            <Icon type="mail" />: {currentUser.email}
+          </h1>
         </div>
       </div>
     );

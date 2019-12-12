4b9337958d4d794e314466122bf1b92b3a8aace6
llf <llf@email.com>
2019-12-12 14:44:36 +0800

    已经改好了个人信息

--- frontend/src/pages/Personal/components/BasicSetting.js
+++ frontend/src/pages/Personal/components/BasicSetting.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
-import { getInfo } from '@/services/user';
+import { connect } from 'dva';
 
+@connect(({ user, loading }) => ({
+  user,
+  submitting: loading.effects['user/fetchCurrent'],
+}))
 export default class BasicSetting extends Component {
   constructor(props) {
+    console.log(props);
     super(props);
-    this.state = {
-      name: 'name',
-      email: 'email',
-    };
-  }
-
-  async componentDidMount() {
-    const resp = await getInfo();
-    this.setState({
-      name: resp.data.name,
-      email: resp.data.email,
-    });
   }
 
   render() {
+    const {
+      user: { currentUser },
+    } = this.props;
     return (
       <div>
         <div style={{ border: '1px dashed', width: 260, padding: '10px' }}>
-          <h3>用户名：{this.state.name}</h3>
-          <h3>邮箱: {this.state.email}</h3>
+          <h3>用户名：{currentUser.name}</h3>
+          <h3>邮箱: {currentUser.email}</h3>
         </div>
       </div>
     );

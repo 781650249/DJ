04b56cf575665d545fe481192d6ba4f7e6043f85
llf <llf@email.com>
2019-12-19 16:48:08 +0800

    api接口

--- frontend/src/services/Goods.js
+++ frontend/src/services/Goods.js
@@ -1,15 +1,25 @@
 import request from '@/utils/request';
 import { stringify } from 'qs';
 
+/*
+获取商品接口
+*/
 export async function getGoods(params) {
   return request(`/api/products?${stringify(params)}`);
 }
+
+/*
+删除商品接口
+*/
 export async function delGoods(id) {
   return request(`/api/products/${id.id}`, {
     method: 'DELETE',
   });
 }
 
+/*
+修改商品接口
+*/
 export async function updGoods(params) {
   return request(`/api/products/${params.id}`, {
     method: 'PUT',
@@ -17,6 +27,9 @@ export async function updGoods(params) {
   });
 }
 
+/*
+添加商品接口
+*/
 export async function addGoods(params) {
   return request('/api/products', {
     method: 'POST',
@@ -24,19 +37,23 @@ export async function addGoods(params) {
   });
 }
 
+/*
+批量删除接口
+*/
 export async function bdelGoods(params) {
   return request('/api/products/batch_delete', {
     method: 'POST',
     data: params,
   });
 }
+
+/*
+文件上传接口
+*/
 export const formData = async (resource, params) => {
-  const a = resource.toString().split('/');
-  const b = a[0].replace('Goods', 'products');
-  const res = `${b}/import`;
   const fileForm = new FormData();
   Object.keys(params).map(key => fileForm.append(key, params[key]));
-  return request(`/api/${res}`, {
+  return request(`/api/${resource}`, {
     method: 'post',
     data: fileForm,
   });

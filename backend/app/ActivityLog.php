70d9021c888915ebeccc79e989320d2ca6ac6634
niming175 <niming175@qq.com>
2019-12-26 17:06:09 +0800

    修改，显示日志中文字段

--- backend/app/ActivityLog.php
+++ backend/app/ActivityLog.php
@@ -13,6 +13,10 @@ class ActivityLog extends Model
         'properties'    => 'json'
     ];
 
+    protected $appends = [
+        'type_name'
+    ];
+
     //
     const TYPE_AUTH_TYPE_CHANGE_PASSWORD = 'CHANGE_PASSWORD';   // 修改密码
 
@@ -60,6 +64,54 @@ class ActivityLog extends Model
     const TYPE_SHIPPING_IMPORT_UPDATE = 'SHIPPING_IMPORT_UPDATE';
     const TYPE_SHIPPING_IMPORT_FAILED = 'SHIPPING_IMPORT_FAILED';
 
+    const LOG_TYPE = [
+        self::TYPE_AUTH_TYPE_CHANGE_PASSWORD => '修改密',
+
+        /************ 商品管理 ***************/
+        self::TYPE_PRODUCT_ADD               => '添加商',
+        self::TYPE_PRODUCT_UPDATE            => '更新商',
+        self::TYPE_PRODUCT_DELETE            => '删除商',
+        self::TYPE_PRODUCT_IMPORT            => '批量导',
+        self::TYPE_PRODUCT_IMPORT_FAILED     => '批量导入失',
+        self::TYPE_PRODUCT_IMPORT_ADD        => '导入添',
+        self::TYPE_PRODUCT_IMPORT_UPDATE     => '导入更',
+        self::TYPE_PRODUCT_BATCH_DELETE      => '批量删除中删除商',
+        self::TYPE_PRODUCTS_BATCH_DEL        => '商品批量删',
+
+        /************* 订单管理 *****************/
+        self::TYPE_ORDER_ADD                 => '添加订',
+        self::TYPE_IMPORT_ORDER              => '导入订',
+        self::TYPE_IMPORT_ORDER_FAILED       => '导入订单失败',
+        self::TYPE_IMPORT_ORDER_CREATE       => '导入时创建订单',
+        self::TYPE_IMPORT_ORDER_UPDATE       => '导入时更新订单',
+
+        self::TYPE_ORDER_UPDATE_STATUS            => '更新订单状态',
+        self::TYPE_ORDER_MARK_URGENT              => '标记为加',
+        self::TYPE_ORDER_BATCH_MARK_URGENT        => '批量标记加',
+        self::TYPE_ORDER_BATCH_MARK_URGENT_FAILED => '批量标记失',
+        self::TYPE_ORDER_CANCEL_URGENT            => '取消加',
+        self::TYPE_ORDER_BATCH_CANCEL_URGENT      => '批量取消加',
+
+        /************* customer ****************/
+        self::TYPE_CUSTOMER_ADD                   => '添加顾客信息',
+        self::TYPE_CUSTOMER_UPDATE                => '更新顾客信息',
+        self::TYPE_CUSTOMER_DELETE                => '删除顾客信息',
+        self::TYPE_IMPORT_ORDER_CREATE_CUSTOMER   => '导入时创建顾客信息',
+        self::TYPE_IMPORT_ORDER_UPDATE_CUSTOMER   => '导入时更新顾客信息',
+
+        /************* file ********************/
+        self::TYPE_DOWNLOAD_ZIP                   => '下载原始zip文',
+        self::TYPE_DOWNLOAD_ZIP_FAILED            => '下载zip文件失',
+        self::TYPE_UNZIP_FILE                     => '解压zip文',
+        self::TYPE_UNZIP_FILE_FAILED              => '解压zip文件失',
+
+        /************ shipping ****************/
+        self::TYPE_SHIPPING_IMPORT                => '导入订单物流',
+        self::TYPE_SHIPPING_IMPORT_ADD            => '导入时创建物流信息',
+        self::TYPE_SHIPPING_IMPORT_UPDATE         => '导入时更新物流信息',
+        self::TYPE_SHIPPING_IMPORT_FAILED         => '导入订单错误',
+    ];
+
     public function causer(): MorphTo {
         return $this->morphTo('causer', 'causer_type', 'causer_id');
     }
@@ -68,4 +120,7 @@ class ActivityLog extends Model
         return $this->morphTo('subject', 'subjetc_type', 'subject_id');
     }
 
+    public function getTypeNameAttribute() {
+        return self::LOG_TYPE[$this->log_name] ?? $this->log_name;
+    }
 }

1d97723e4c0f4d9d891bb8d3ab04a603f1f0d99f
llf <llf@email.com>
2019-12-26 16:04:41 +0800

    导入订单

---
+++ frontend/src/components/OrderManage/LoadOrder.js
@@ -0,0 +1,55 @@
+import { Modal, Button } from 'antd';
+import React from 'react';
+import LoadExcel from './components/LoadExcel';
+
+class LoadOrders extends React.Component {
+  state = {
+    visible: false,
+    confirmLoading: false,
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = () => {
+    this.setState({
+      confirmLoading: true,
+    });
+
+    this.setState({
+      visible: false,
+      confirmLoading: false,
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    const { visible, confirmLoading } = this.state;
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          导入订单
+        </Button>
+        <Modal
+          maskClosable={false}
+          title="导入订单"
+          visible={visible}
+          confirmLoading={confirmLoading}
+          onCancel={this.handleCancel}
+          footer={[]}
+        >
+          <LoadExcel props={this.state} onCancel={this.handleCancel} />
+        </Modal>
+      </div>
+    );
+  }
+}
+export default LoadOrders;

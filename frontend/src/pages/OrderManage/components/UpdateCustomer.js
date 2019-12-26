a5f958bf2250f701b1245575e7ef7810f67e2e70
llf <llf@email.com>
2019-12-26 17:53:55 +0800

    图片modal

---
+++ frontend/src/pages/OrderManage/components/UpdateCustomer.js
@@ -0,0 +1,204 @@
+import { Button, Modal, Form, Input, notification } from 'antd';
+import { connect } from 'dva';
+import React from 'react';
+
+const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
+  // eslint-disable-next-line react/prefer-stateless-function
+  class extends React.Component {
+    render() {
+      const { visible, onCancel, onCreate, form, data = {}, loading } = this.props;
+      const { getFieldDecorator } = form;
+      const formItemLayout = {
+        labelCol: {
+          xs: { span: 12 },
+          sm: { span: 6 },
+        },
+        wrapperCol: {
+          xs: { span: 12 },
+          sm: { span: 18 },
+        },
+      };
+
+      return (
+        <Modal
+          maskClosable={false}
+          visible={visible}
+          title="修改顾客信息"
+          okText="保存"
+          onCancel={onCancel}
+          onOk={onCreate}
+          confirmLoading={loading}
+        >
+          <div style={{ padding: 12 }}>
+            <Form {...formItemLayout} layout="vertical">
+              <Form.Item label="姓名">
+                {getFieldDecorator('name', {
+                  initialValue: data.name,
+                  rules: [{ required: true, message: '姓名不能为空' }],
+                })(<Input type="textarea" placeholder="请输入姓名" />)}
+              </Form.Item>
+
+              <Form.Item label="邮箱">
+                {getFieldDecorator('email', {
+                  initialValue: data.email,
+                  rules: [{ required: true, message: '邮箱不能为空' }],
+                })(<Input placeholder="请输入邮箱" />)}
+              </Form.Item>
+
+              <Form.Item label="phone">
+                {getFieldDecorator('phone', {
+                  initialValue: data.phone,
+                  rules: [{ max: 32, message: '最大长度不能超过32' }],
+                })(<Input placeholder="请输入手机号" />)}
+              </Form.Item>
+
+              <Form.Item label="国家">
+                {getFieldDecorator('country', {
+                  initialValue: data.country,
+                  rules: [{ max: 32, message: '最大长度不能超过32' }],
+                })(<Input placeholder="请输入国家" />)}
+              </Form.Item>
+
+              <Form.Item label="州/省">
+                {getFieldDecorator('province', {
+                  initialValue: data.province,
+                  rules: [{ max: 32, message: '最大长度不能超过32' }],
+                })(<Input placeholder="请输入国家" />)}
+              </Form.Item>
+
+              <Form.Item label="城市">
+                {getFieldDecorator('city', {
+                  initialValue: data.city,
+                  rules: [{ max: 32, message: '最大长度不能超过32' }],
+                })(<Input placeholder="请输入城市" />)}
+              </Form.Item>
+
+              <Form.Item label="邮编">
+                {getFieldDecorator('zip_code', {
+                  initialValue: data.zip_code,
+                  rules: [{ max: 16, message: '最大长度不能超过16' }],
+                })(<Input placeholder="请输入邮编" />)}
+              </Form.Item>
+
+              <Form.Item label="地址1">
+                {getFieldDecorator('address1', {
+                  initialValue: data.address1,
+                  rules: [{ max: 128, message: '最大长度不能超过128' }],
+                })(<Input placeholder="请输入地址1" />)}
+              </Form.Item>
+
+              <Form.Item label="地址2">
+                {getFieldDecorator('address2', {
+                  initialValue: data.address2,
+                  rules: [{ max: 128, message: '最大长度不能超过128' }],
+                })(<Input placeholder="请输入地址2" />)}
+              </Form.Item>
+
+              <Form.Item label="地址3">
+                {getFieldDecorator('address3', {
+                  initialValue: data.address3,
+                  rules: [{ max: 128, message: '最大长度不能超过128' }],
+                })(<Input placeholder="请输入地址3" />)}
+              </Form.Item>
+            </Form>
+          </div>
+        </Modal>
+      );
+    }
+  },
+);
+
+@connect(({ orders, loading }) => ({
+  orders,
+  submitting: loading.effects['orders/updateorders'],
+}))
+export default class Updateorders extends React.Component {
+  state = {
+    visible: false,
+    loading: false,
+  };
+
+  showModal = () => {
+    this.setState({ visible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  handleCreate = () => {
+    const { form } = this.formRef.props;
+    const {
+      dispatch,
+      data: { id },
+    } = this.props;
+    console.log(this.props);
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+      this.setState({
+        loading: true,
+      });
+      dispatch({
+        type: 'orders/updateorders',
+        payload: {
+          id,
+          ...values,
+        },
+        callback: response => {
+          if (response.response.status === 200) {
+            notification.success({
+              message: '修改成功',
+            });
+            // 刷新
+            dispatch({
+              type: 'orders/fetchOrders',
+              payload: {
+                page: 1,
+                page_size: '',
+              },
+            });
+            form.resetFields();
+            dispatch({
+              type: 'Goods/fetchOrders',
+            });
+          }
+          this.setState({
+            loading: false,
+          });
+          this.setState({ visible: false });
+        },
+      });
+    });
+  };
+
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+
+  render() {
+    const { data } = this.props;
+    const { loading, errorMessage } = this.state;
+    return (
+      <>
+        <Button
+          icon="edit"
+          title="编辑此条记录"
+          shape="circle"
+          size="small"
+          onClick={this.showModal}
+        ></Button>
+        <CollectionCreateForm
+          errorMessage={errorMessage}
+          data={data}
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCreate={this.handleCreate}
+          onCancel={this.handleCancel}
+          loading={loading}
+        />
+      </>
+    );
+  }
+}
---
+++ frontend/src/pages/OrderManage/index.jsx
@@ -0,0 +1,205 @@
+import React, { Component } from 'react';
+import { Table, Button, Alert } from 'antd';
+import Header from './components/Header';
+// eslint-disable-next-line import/no-unresolved
+import UpdateCustomer from './components/updateCustomer';
+import UpdOrderStatus from './components/UpdOrderStatus';
+import OriginImg from './components/OriginImg';
+import SingHurryOrder from './components/SingHurryOrder';
+import HurryOrder from './components/HurryOrder';
+import { connect } from 'dva';
+
+@connect(({ orders, loading }) => ({
+  orders,
+  submitting: loading.effects['orders/fetchOrders'],
+}))
+export default class Content extends Component {
+  state = {
+    selectedRowKeys: [],
+    id: [],
+  };
+
+  componentDidMount() {
+    this.initOrder();
+  }
+
+  initOrder = () => {
+    const { dispatch } = this.props;
+    const {
+      result: { filter },
+    } = this.props.orders;
+    dispatch({
+      type: 'orders/fetchOrders',
+      payload: {
+        page: 1,
+        page_size: 10,
+        filter: {
+          ...filter,
+        },
+      },
+    });
+  };
+
+  start = () => {
+    this.setState({
+      selectedRowKeys: [],
+    });
+  };
+
+  bDelete = () => {};
+
+  onSelectChange = selectedRowKeys => {
+    this.setState({ selectedRowKeys });
+  };
+
+  render() {
+    const columns = [
+      {
+        dataIndex: 'urgent',
+        render: (_, data) => <SingHurryOrder data={data} />,
+      },
+      { title: '订单编号', dataIndex: 'oid', key: 'oid', width: '100px' },
+      { title: '订单号', dataIndex: 'number', key: 'number' },
+      { title: '订单时间', dataIndex: 'order_date', key: 'order_date', width: '100px' },
+      { title: '订单状态', dataIndex: 'order_status', key: 'order_status', width: '100px' },
+      { title: '发稿时间', dataIndex: 'published_at', key: 'published_at' },
+      { title: '生产时间', dataIndex: 'produced_at', key: 'produced_at' },
+      { title: 'sku', dataIndex: 'sku', key: 'sku' },
+      {
+        title: '原始素材',
+        dataIndex: 'un_zip_files',
+        key: 'un_zip_files',
+        width: '200px',
+        render: files => <OriginImg files={files} />,
+      },
+      { title: '订单备注', dataIndex: 'note', key: 'note', width: '100px' },
+      { title: '销售量', dataIndex: 'quantity', key: 'quantity' },
+    ];
+
+    const { submitting } = this.props;
+    const { selectedRowKeys, id } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+      id,
+    };
+    const hasSelected = selectedRowKeys.length > 0;
+    const {
+      orders: { result },
+    } = this.props;
+    const { data: datas, total, page } = result;
+    const columnSon = [
+      {
+        title: '姓名',
+        dataIndex: 'name',
+      },
+      {
+        title: '邮箱',
+        dataIndex: 'email',
+      },
+      {
+        title: '电话',
+        dataIndex: 'phone',
+      },
+      {
+        title: '国家',
+        dataIndex: 'country',
+      },
+      {
+        title: '省份',
+        dataIndex: 'province',
+      },
+      {
+        title: '地区',
+        dataIndex: 'city',
+      },
+      {
+        title: '地址',
+        dataIndex: 'address1',
+      },
+      {
+        title: '邮编',
+        dataIndex: 'zip_code',
+      },
+      {
+        title: '操作',
+        dataIndex: 'id',
+        render: (_, item) => (
+          <>
+            <UpdateCustomer style={{ marginLeft: '10px' }} data={item} />
+          </>
+        ),
+      },
+    ];
+    return (
+      <div>
+        <Header />
+        <div style={{ marginTop: 20 }}>
+          <Alert
+            message={
+              <span>
+                <Button size="small" disabled={!hasSelected} type="primary" onClick={this.start}>
+                  取消全选
+                </Button>
+                <Button
+                  disabled={!hasSelected}
+                  size="small"
+                  style={{ marginLeft: 2 }}
+                  type="danger"
+                  onClick={this.bDelete}
+                >
+                  删除
+                </Button>
+                <span style={{ marginLeft: 12 }}>
+                  {hasSelected ? `当前选中了 ${selectedRowKeys.length} 项` : ''}
+                </span>
+              </span>
+            }
+            type="info"
+            showIcon
+          ></Alert>
+          <Alert
+            message={
+              <div style={{ display: 'flex', verticalAlign: 'middle' }}>
+                <HurryOrder hasSelected={hasSelected} id={selectedRowKeys}>
+                  加急标记
+                </HurryOrder>
+                <UpdOrderStatus hasSelected={hasSelected} id={selectedRowKeys} />
+              </div>
+            }
+            type="error"
+            showIcon
+          />
+
+          <Table
+            loading={submitting}
+            pagination={{
+              showQuickJumper: true,
+              current: page,
+              total,
+              onChange: this.changeNum,
+              showTotal: totals => `总共有${totals}条记录`,
+            }}
+            size="small"
+            expandRowByClick={false}
+            rowSelection={rowSelection}
+            columns={columns}
+            rowKey="id"
+            dataSource={datas}
+            expandedRowRender={record => (
+              <Table
+                rowKey="id"
+                bordered
+                style={{ margin: '12px 0' }}
+                columns={columnSon}
+                dataSource={[record.customer]}
+                pagination={false}
+                size="small"
+              />
+            )}
+          />
+        </div>
+      </div>
+    );
+  }
+}

4f93564bdc564d49f4409782661d6dc22c2b3da0
niming175 <niming175@qq.com>
2019-12-30 15:56:29 +0800

    增加下拉菜单的示例

---
+++ frontend/src/pages/ExamplePage/components/ButtomModel.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import { Modal, notification } from 'antd';
+import { setInterval } from 'timers';
+
+@connect(() => ({}))
+class ButtonModel extends Component {
+  state = {
+    visible: false,
+    loading: false,
+  };
+
+  componentWillUnmount() {
+    clearInterval();
+  }
+
+  open = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  cancel = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleOk = async () => {
+    this.setState({
+      loading: true,
+    });
+
+    await this.success();
+  };
+
+  success = () => {
+    setTimeout(() => {
+      notification.success({
+        message: 'success',
+        description: 'success',
+      });
+
+      this.setState({
+        loading: false,
+        visible: false,
+      });
+    }, 1000);
+  };
+
+  render() {
+    const { visible, loading } = this.state;
+    const { children, color } = this.props;
+
+    return (
+      <div>
+        <div style={{ color }} onClick={this.open}>
+          . {children}
+        </div>
+        <Modal
+          visible={visible}
+          title="确认修改"
+          onCancel={this.cancel}
+          onOk={this.handleOk}
+          confirmLoading={loading}
+        >
+          <p>
+            确定将状态修改为
+            <b style={{ color }}>{children}</b>?
+          </p>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ButtonModel;

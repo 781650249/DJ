351b4eeed65462e840fbf4cf6bd2e23bc3bf94dc
niming175 <niming175@qq.com>
2019-12-30 16:13:23 +0800

    修改细节

--- frontend/src/pages/ExamplePage/components/ButtomModel.js
+++ frontend/src/pages/ExamplePage/components/ButtomModel.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import { Modal, notification } from 'antd';
-import { setInterval } from 'timers';
 
 @connect(() => ({}))
 class ButtonModel extends Component {
